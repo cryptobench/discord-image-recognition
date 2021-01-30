86b27311f17c047816745ec83ca9e2b912ccb510
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-01-30 18:34:08 +0100

    Rotate and generate noise on images

--- generate_data.sh
+++ generate_data.sh
@@ -1,8 +1,10 @@
+DISCORD_ADMIN="mp"
+
 for run in {1..50}
 do
   NUMBER=$(jot -r 1  1 35)
+  ROTATE=$(jot -r 1  -35 35)
   RAND=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
   RANDNAMEROTATE=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-  convert +noise Gaussian -evaluate add "${NUMBER}%" admin_images/etam.png "data/train/etam/${RAND}.png"
-  convert admin_images/etam.png -rotate ${NUMBER} data/train/etam/${RANDNAMEROTATE}.png
+  convert admin_images/${DISCORD_ADMIN}.png -rotate ${ROTATE} data/train/${DISCORD_ADMIN}/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/train/${DISCORD_ADMIN}/${RANDNAMEROTATE}.png "data/train/${DISCORD_ADMIN}/${RANDNAMEROTATE}.png"
 done
