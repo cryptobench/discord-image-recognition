2b47bfef332bc15d9237a6c427c1e1feca7e66d8
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-01-30 18:51:02 +0100

    Update dataDirectory

--- train.py
+++ train.py
@@ -2,6 +2,6 @@ from imageai.Classification.Custom import ClassificationModelTrainer
 
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsDenseNet121()
-model_trainer.setDataDirectory("admins")
+model_trainer.setDataDirectory("data")
 model_trainer.trainModel(num_objects=8, num_experiments=300, enhance_data=True, batch_size=32, show_network_summary=True)
 
