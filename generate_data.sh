08cb2ef4c86b551f28cee3f86c4c5ee3952d4b3f
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-01-30 21:02:14 +0100

    Easier data generation

--- generate_data.sh
+++ generate_data.sh
@@ -1,10 +1,12 @@
-DISCORD_ADMIN="mp"
+declare -a StringArray=("2rec" "blueshade" "costanza" "etam" "kuba" "mattias" "mp" "phillip")
 
-for run in {1..50}
-do
-  NUMBER=$(jot -r 1  1 35)
-  ROTATE=$(jot -r 1  -35 35)
-  RAND=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-  RANDNAMEROTATE=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-  convert admin_images/${DISCORD_ADMIN}.png -rotate ${ROTATE} data/train/${DISCORD_ADMIN}/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/train/${DISCORD_ADMIN}/${RANDNAMEROTATE}.png "data/train/${DISCORD_ADMIN}/${RANDNAMEROTATE}.png"
+for run in {1..50}; do
+
+  for admin in ${StringArray[@]}; do
+    NUMBER=$(jot -r 1 1 35)
+    ROTATE=$(jot -r 1 -35 35)
+    RAND=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
+    RANDNAMEROTATE=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
+    convert admin_images/${admin}.png -rotate ${ROTATE} data/train/${admin}/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/train/${admin}/${RANDNAMEROTATE}.png "data/train/${admin}/${RANDNAMEROTATE}.png"
+  done
 done
