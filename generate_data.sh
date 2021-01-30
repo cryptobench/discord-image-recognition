29bb20fb0075c76400d2b00ebfc639dfa2593ff0
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-01-30 18:04:59 +0100

    Small fix to the script.

--- generate_data.sh
+++ generate_data.sh
@@ -3,6 +3,6 @@ do
   NUMBER=$(jot -r 1  1 35)
   RAND=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
   RANDNAMEROTATE=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-  convert +noise Gaussian -evaluate add "${NUMBER}%" etam.png "etam/${RAND}.png"
-  convert etam/${RAND}.png -rotate ${NUMBER} etam/${RANDNAMEROTATE}.png
+  convert +noise Gaussian -evaluate add "${NUMBER}%" etam.png "data/train/etam/${RAND}.png"
+  convert etam.png -rotate ${NUMBER} data/train/etam/${RANDNAMEROTATE}.png
 done
