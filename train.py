31b397a429e5e1512da24d2379d481b65417c9fe
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-03-01 14:25:01 +0100

    Remove old file

--- train.py
+++
@@ -1,7 +0,0 @@
-from imageai.Classification.Custom import ClassificationModelTrainer
-
-model_trainer = ClassificationModelTrainer()
-model_trainer.setModelTypeAsInceptionV3()
-model_trainer.setDataDirectory("data")
-model_trainer.trainModel(num_objects=2, continue_from_model="data/models/model_ex-283_acc-0.999897.h5", transfer_with_full_training=True, training_image_size=250, num_experiments=300, enhance_data=False, batch_size=32, show_network_summary=True)
-
