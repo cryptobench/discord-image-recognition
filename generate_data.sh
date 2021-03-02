27178fb08683f1e8c182e780da1f2bda1733aadb
Phillip Jensen <phillip@golemgrid.com>
2021-03-03 00:54:30 +0100

    Crop % into each training image

--- generate_data.sh
+++ generate_data.sh
@@ -8,26 +8,32 @@ for admin in ${StringArray[@]}; do
 done
 
 # Modify integers below to decide how many images to generate in the training folders.
-for run in {1..500}; do
+for run in {1..2000}; do
 
   for admin in ${StringArray[@]}; do
     NUMBER=$(jot -r 1 1 35)
     ROTATE=$(jot -r 1 -35 35)
+    X=$(jot -r 1 80 99)
+    Y=$(jot -r 1 80 99)
+    DIRECTION=$(shuf -n1 directions.txt | cut -d$'\t' -f1)
     RAND=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
     RANDNAMEROTATE=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-    convert admin_images/${admin}.png -rotate ${ROTATE} data/train/admin/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/train/admin/${RANDNAMEROTATE}.png "data/train/admin/${RANDNAMEROTATE}.png"
+    convert admin_images/${admin}.png -rotate ${ROTATE} data/train/admin/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/train/admin/${RANDNAMEROTATE}.png "data/train/admin/${RANDNAMEROTATE}.png" && convert "data/train/admin/${RANDNAMEROTATE}.png" -gravity ${DIRECTION} -crop ${X}%x${Y}%+0+0 "data/train/admin/${RANDNAMEROTATE}.png"
   done
 done
 
 # Modify integers below to decide how many images to generate in the test folders.
-for run in {1..100}; do
+for run in {1..400}; do
 
   for admin in ${StringArray[@]}; do
     NUMBER=$(jot -r 1 1 35)
     ROTATE=$(jot -r 1 -35 35)
+    X=$(jot -r 1 80 99)
+    Y=$(jot -r 1 80 99)
+    DIRECTION=$(shuf -n1 directions.txt | cut -d$'\t' -f1)
     RAND=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
     RANDNAMEROTATE=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-    convert admin_images/${admin}.png -rotate ${ROTATE} data/test/admin/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/test/admin/${RANDNAMEROTATE}.png "data/test/admin/${RANDNAMEROTATE}.png"
+    convert admin_images/${admin}.png -rotate ${ROTATE} data/test/admin/${RANDNAMEROTATE}.png && convert +noise Gaussian -evaluate add "${NUMBER}%" data/test/admin/${RANDNAMEROTATE}.png "data/test/admin/${RANDNAMEROTATE}.png" && convert "data/test/admin/${RANDNAMEROTATE}.png" -gravity ${DIRECTION} -crop ${X}%x${Y}%+0+0 "data/test/admin/${RANDNAMEROTATE}.png"
   done
 done
 
